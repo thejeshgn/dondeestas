bdfdcc5a8404686fad623f1d9a115b9a77bf1ca6
Varunity <varunity@varunity-latitude.(none)>
2014-05-27 09:29:20 -0500

    added bootstrap, static files and homepage template

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,17 +3,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin,auth
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+import misc.views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'main.views.WelcomeHome'),
-
      url(r'^api/', include('api.urls')),
 
     #admin related urls
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin_tools/', include('admin_tools.urls')),
+#    url(r'^admin_tools/', include('admin_tools.urls')),
+    url(r'^$', misc.views.Home.as_view(),
+        name='home',),
 
 )
 
--- src/misc/views.py
+++ src/misc/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.response import TemplateResponse
+
+class Home(View):
+
+    def get(self, request, *args, **kwargs):
+        return TemplateResponse(request, 'home.html')
+
