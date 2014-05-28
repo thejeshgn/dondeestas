e2ada851b54d1ec3f57cf72785f14e24577d3f23
thejeshgn <i@thejeshgn.com>
2014-05-28 19:29:00 +0530

    fixed

--- misc/views.py
+++ misc/views.py
@@ -7,13 +7,14 @@ from django.http import HttpResponse
 from django.views.generic import View
 from django.template.response import TemplateResponse
 from models import Migrant, CheckPoint
-
+import json
 
 
 class DisplayPath(View):
     def post(self, request, *args, **kwargs):
         pseudo_name =  request.POST.get('searchname',None) 
         errors = []
+        migrant = None
         if pseudo_name:
             try:
                 migrant = Migrant.objects.get(pseudo=pseudo_name)
@@ -23,8 +24,23 @@ class DisplayPath(View):
                 #log, the guy who searched, ip, pseudo name, type of search
                 #might required in future for showing captcha or banning
                 return redirect('/', errors=errors)
+
             checkpoints = CheckPoint.objects.filter(migrante=migrant)
-            return render_to_response('display_migrant_location.html',{'migrant':migrant,'checkpoints':checkpoints,'errors':errors})
+            lineArray = []
+            point = []
+            point.append(float(migrant.origin_lng))
+            point.append(float(migrant.origin_lat))
+            lineArray.append(point)  
+            for checkpoint in checkpoints:
+                point = []
+                point.append(float(checkpoint.lon))
+                point.append(float(checkpoint.lat))
+                lineArray.append(point)
+            path_covered = json.dumps(lineArray)
+
+
+
+            return render_to_response('display_migrant_location.html',{'migrant':migrant,'path_covered':path_covered,'errors':errors})
         else:
             #error: please enter a pseudo name
             errors.append('Please enter a valid Pseudo name')
