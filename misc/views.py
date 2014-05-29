471262fc896e4406aaeb95cccc86d43b1299702f
thejeshgn <i@thejeshgn.com>
2014-05-29 15:03:58 +0530

    added migrant inbox

--- misc/views.py
+++ misc/views.py
@@ -132,13 +132,104 @@ class CheckPointView(View):
                 #log, the guy who searched, ip, pseudo name, type of search
                 #might required in future for showing captcha or banning
                 return redirect('/', errors=errors)
-            return render_to_response('checkpoint.html',{})
+            return render_to_response('checkpoint.html',{'migrant':migrant})
         else:
             #error: please enter a pseudo name
             errors.append('Please enter a valid Pseudo name')
             #log, the guy who searched, ip, type of search
             #might required in future for showing captcha or banning
             return redirect('/', errors=errors)
+
+    def get(self, request, *args, **kwargs):
+        errors = []
+        errors.append('Pseudo name not found. Please enter a valid one.')
+        return redirect('/',errors=errors)
+
+class CheckPointSave(View):
+    def post(self, request, *args, **kwargs):
+        pseudo_name =  request.POST.get('pseudo_name',None) 
+        latitude =  request.POST.get('latitude',None) 
+        longitude =  request.POST.get('longitude',None) 
+
+        errors = []
+        migrant = None
+        if pseudo_name:
+            try:
+                migrant = Migrant.objects.get(pseudo=pseudo_name)
+            except Migrant.DoesNotExist:
+                #error, just say migrant doesnt exist
+                errors.append('Pseudo name not found. Please enter a valid one.')
+                #log, the guy who searched, ip, pseudo name, type of search
+                #might required in future for showing captcha or banning
+                return redirect('/', errors=errors)
+
+            #if valid migrant, and location
+            if latitude and longitude:
+                checkpoint = CheckPoint()
+                checkpoint.save()
+
+                #future path of the migrant for display
+                checkpoints = CheckPoint.objects.filter(migrante=migrant)
+                point = []
+                point.append(float(migrant.origin_lng))
+                point.append(float(migrant.origin_lat))
+                multiLineArray = []
+                for i in range(0,len(checkpoints)):
+                    lineArray = []
+                    #starting point
+                    if i ==0:
+                        lineArray.append(point)  
+                    else:
+                        checkpoint = checkpoints[i-1]
+                        point = []
+                        point.append(float(checkpoint.lon))
+                        point.append(float(checkpoint.lat))
+                        lineArray.append(point)
+
+                    #endpoint
+                    checkpoint = checkpoints[i]
+                    point = []
+                    point.append(float(checkpoint.lon))
+                    point.append(float(checkpoint.lat))
+                    lineArray.append(point)
+                    multiLineArray.append(lineArray)
+
+
+                path_covered = json.dumps(multiLineArray)
+
+
+                #future path of the migrant
+                lineArray = []
+                no_of_checkpoints = len(checkpoints)
+                if checkpoints and no_of_checkpoints > 0: 
+                    last_checkpoint = checkpoints[no_of_checkpoints-1]
+                    point = []
+                    point.append(float(last_checkpoint.lon))
+                    point.append(float(last_checkpoint.lat))
+                    lineArray.append(point)
+                else:
+                    point = []
+                    point.append(float(migrant.origin_lng))
+                    point.append(float(migrant.origin_lat))
+                    lineArray.append(point)  
+
+                point = []
+                point.append(float(migrant.dest_lng))
+                point.append(float(migrant.dest_lat))
+                lineArray.append(point)  
+                future_path= json.dumps(lineArray)
+                return render_to_response('display_migrant_inbox.html',{'migrant':migrant,'path_covered':path_covered,'future_path':future_path,'errors':errors})
+
+
+        else:
+            #error: please enter a pseudo name
+            errors.append('Please enter a valid Pseudo name')
+            #log, the guy who searched, ip, type of search
+            #might required in future for showing captcha or banning
+            return redirect('/', errors=errors)
+
+
+
 
     def get(self, request, *args, **kwargs):
         errors = []
