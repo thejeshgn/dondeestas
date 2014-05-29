aa541e6a265de98f9dccb48b65d3e6a47262e80d
thejeshgn <i@thejeshgn.com>
2014-05-29 13:08:46 +0530

    added details of checkin

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^$', views.Home.as_view(),),
     url(r'path/', views.DisplayPath.as_view(),name='path',),
     url(r'register/', views.Register.as_view(),name='register',),
-    url(r'checkpoint/', views.CheckPointView.as_view(),name='register',),
+    url(r'checkin/', views.CheckPointView.as_view(),name='register',),
 
 )
 
